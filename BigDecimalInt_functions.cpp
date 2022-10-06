0a9a574f1f290dc22727911813988ae693d3560c
Asmaa Heikal <AsmaaIHeikal@gmail.com>
2022-10-06 18:05:46 +0200

    Update project

--- BigDecimalInt_functions.cpp
+++ BigDecimalInt_functions.cpp
@@ -1,1 +1,34 @@
 #include "BigDecimalInt_functions.h"
+
+BigDecimalInt::BigDecimalInt (const string& decStr){
+
+}
+BigDecimalInt ::BigDecimalInt(int decInt){
+
+}
+BigDecimalInt BigDecimalInt::operator+ (const BigDecimalInt& anotherDec){
+
+}
+BigDecimalInt BigDecimalInt::operator- (const BigDecimalInt& anotherDec){
+
+}
+bool BigDecimalInt::operator< (const BigDecimalInt& anotherDec){
+
+}
+bool BigDecimalInt::operator> (const BigDecimalInt& anotherDec){
+
+}
+bool BigDecimalInt::operator==(const BigDecimalInt& anotherDec){
+
+}
+BigDecimalInt& BigDecimalInt:: operator= (const BigDecimalInt& anotherDec){
+
+}
+int BigDecimalInt::size(){
+
+}
+int BigDecimalInt::sign(){}
+
+ostream &operator << (ostream& out, const BigDecimalInt& b){
+
+}
--- BigDecimalInt_functions.h
+++ BigDecimalInt_functions.h
@@ -1,4 +1,24 @@
 #ifndef BIGDECIMALINT_CLASS_BIGDECIMALINT_FUNCTIONS_H
 #define BIGDECIMALINT_CLASS_BIGDECIMALINT_FUNCTIONS_H
 
+#include <bits/stdc++.h>
+using namespace std;
+
+class BigDecimalInt{
+private:
+    string s;
+public:
+    BigDecimalInt (const string& decStr);
+    BigDecimalInt (int decInt);
+    BigDecimalInt operator+ (const BigDecimalInt& anotherDec);
+    BigDecimalInt operator- (const BigDecimalInt& anotherDec);
+    bool operator< (const BigDecimalInt& anotherDec);
+    bool operator> (const BigDecimalInt& anotherDec);
+    bool operator==(const BigDecimalInt& anotherDec);
+    BigDecimalInt& operator= (const BigDecimalInt& anotherDec);
+    int size();
+    int sign();
+    friend ostream& operator << (ostream& out, const BigDecimalInt& b);
+};
+
 #endif //BIGDECIMALINT_CLASS_BIGDECIMALINT_FUNCTIONS_H
